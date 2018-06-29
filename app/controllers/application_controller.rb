7906fed11b403c6ab64f803820d328bf31241bef
jjy <jjyruby@gmail.com>
2018-06-29 18:20:16 +0800

    enable CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::API
+
+  before_action :disable_cors
+
   def authenticate_user
     @user = User.find_by_token!(params[:token])
   end
@@ -7,5 +10,12 @@ class ApplicationController < ActionController::API
     @hackathon ||= Hackathon.find(params[:hackathon_id])
     @hackathon.refresh_real_status
     render("hackathon status error", status: 401) unless statuses.include?(@hackathon.status)
+  end
+
+  def disable_cors
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
